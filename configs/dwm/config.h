2da0a6b7c122893b3a79b4a12795cb9cc969765b
maciejzj <macejzg@gmail.com>
2021-04-04 00:33:11 +0200

    Fix st name in dwm swallow config

--- configs/dwm/config.h
+++ configs/dwm/config.h
@@ -28,9 +28,9 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class     instance  title  tags mask  isfloating  isterminal  noswallow  monitor */
-	{ "st",      NULL,     NULL,  0,         0,          1,           0,        -1 },
-	{ "tabbed",  NULL,     "st",  0,         0,          1,           0,        -1 },
+	/* class          instance  title  tags mask  isfloating  isterminal  noswallow  monitor */
+	{ "st-256color",  NULL,     NULL,  0,         0,          1,           0,        -1 },
+	{ "tabbed",       NULL,     "st",  0,         0,          1,           0,        -1 },
 };
 
 /* layout(s) */
